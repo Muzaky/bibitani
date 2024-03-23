a42934ceff61780a5dc505572cb0f1f284417a69
Muzaky <afif050503@gmail.com>
2024-03-24 02:54:28 +0700

    reworking the landing page

--- resources/views/Layout/Landing.blade.php
+++ resources/views/Layout/Landing.blade.php
@@ -4,36 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BibiTani</title>
-    <link rel="icon" href="LogoBibitani.png">
+    <link rel="icon" href="bibitani.ico">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css">
     <script src="https://cdn.ckeditor.com/ckeditor5/41.2.1/classic/ckeditor.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-      <div class="flex lg:flex-1 justify-center">
-        <a href="#" class="-m-1.5 p-1.5">
-          <span class="sr-only"></span>
-          <img class="h-10 w-auto" src="LogoBibitani.png" alt="">
+      <div class="flex lg:flex-1">
+        <a href="#" class="-m-1.5 p-1.5"> 
+          <span class="sr-only">Your Company</span>
+          <img class="" style="width: 270;height:86px" src="bibitani.ico" alt="logobibitani">
         </a>
       </div>
-      <div class="flex lg:hidden">
-        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-          <span class="sr-only">Open main menu</span>
-          <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-        </button>
-      </div>
       <div class="lg:flex lg:gap-x-12">
-        <a href="/" class="text-sm font-semibold leading-6 text-gray-900 ">Home</a>
-        <a href="{{route('berita.landing')}}" class="text-sm font-semibold leading-6 text-gray-900">Berita</a>
-        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Pengajuan</a>
+        <a href="/" class="font-semibold leading-6 text-gray-900" style="font-size:20px">Home</a>
+        <a href="{{route('berita.landing')}}" class="font-semibold leading-6 text-gray-900" style="font-size:20px">Berita</a>
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900" style="font-size:20px">Pengajuan</a>
       </div>
-      <div class="hidden lg:flex lg:flex-1 lg:justify-center">
-        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+      <div class=" lg:flex lg:flex-1 lg:justify-end">
+        <a href="#" class="flex justify-center text-sm font-semibold leading-6 text-gray-900 mx-8" style="font-size:20px; width: 128px; height:44px;background-color:#204E51; color:#f4f4f4; align-items: center;border-radius:8px">Login</a>
+        <a href="#" class="flex justify-center text-sm font-semibold leading-6 text-gray-900" style="font-size:20px; width: 128px; height:44px;background-color:#f4f4f4; color:#204E51; align-items: center;border-radius:8px; border :1px solid #204E51">Sign Up</a>
       </div>
     </nav>
+    <!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     @yield('content');
 </body>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,19 +1,36 @@
 @extends('Layout.Landing')
 @section('content')
+<style>
+  #overlay {
+  
+  background-color: rgba(0,0,0,0.5);
+  background-size:cover;
+
+}
+</style>
 <section>
-  <div class="relative isolate px-6 lg:px-8">
-      <div class=" inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-          <div class="relative left-[calc(50%-11rem)] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#34a0a4] to-[#d9ed92] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-      </div>
-    <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+
+<div class="relative isolate px-6 lg:px-8 bg-cover h-screen" style="background-image: url(image_1.png); background-size: cover;background-repeat: no-repeat;" >
+  <div class="h-screen bg-cover -mx-8"id="overlay">
+    <div class="mx-auto max-w-4xl py-32 sm:py-48 lg:py-40">
       <div class="text-center">
-        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Selamat Datang di <font color="#d9ed92">BibiTani !</font></h1>
-        <p class="mt-6 text-lg leading-8 text-gray-600">Lakukan pengajuan subsidi bibit tanpa harus datang ke dinas !</p>
+        <h1 class="font-bold tracking-tight sm:text-6xl style" style="font-size:90px ;color:#f4f4f4">Solusi Penyaluran Bibit <font color="#39A413">Horikultura</font> Kabupaten Jember</h1>
+        <p class="mt-6 text-lg leading-8" style="color:#f4f4f4">Lakukan pengajuan subsidi bibit tanpa harus datang ke dinas !</p>
       </div>
-    </div>
-    <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-      <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#d9ed92] to-[#34a0a4] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+      <div class="mt-10 flex items-center justify-center gap-x-6">
+          <a href="#" class="flex justify-center items-center text-sm font-semibold leading-6 text-gray-900" style="font-size:20px; width: 176px; height:48px;background-color:#transparent; color:#f4f4f4;border-radius:8px; border :1px solid #f4f4f4">Learn more</a>
+        </div>
     </div>
   </div>
+    <!-- <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+      <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#d9ed92] to-[#34a0a4] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+    </div> -->
+</div>
+
+<!-- <div class="flex" style="height: 600px; background-color: #204E51 ">
+  <div class="flex mx-auto max-w-7xl px-6 lg:px-8 text-center mt-6">
+    <h2 class="flex text-3xl font-semibold text-white sm:text-4xl">Berita Terkini</h2>
+  </div>
+</div> -->
 </section>
 @endsection
